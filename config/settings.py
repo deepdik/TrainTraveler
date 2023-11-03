e9a42a4d08ad16757bb90e8e31ab7f75d7d03938
Deepak kumar <dk5f95@gmail.com>
2023-11-03 16:33:40 -0500

    added allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f0f19&)ro)!)zb*yb1y9c6)*_84+8im^xb5f5h@fp9p0od(bh@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
